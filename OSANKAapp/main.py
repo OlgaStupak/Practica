c7139f6e738d392a7f7df327b3546ae6877cdae8
OlgaStupak <oastupak@mai.education>
2023-07-20 14:06:12 +0300

    update app

--- OSANKAapp/main.py
+++ OSANKAapp/main.py
@@ -8,7 +8,9 @@ from time import sleep
 mp_drawing = mp.solutions.drawing_utils
 mp_pose = mp.solutions.pose
 
-cap = cv2.VideoCapture(0)  #импорт видеопотока
+bad_time = 150 #примерно 10 секунд
+
+cap = cv2.VideoCapture(1)  #импорт видеопотока
 
 counter = 0  #счётчик
 stage = None #состояние сгибания (нейтральное)
@@ -28,16 +30,10 @@ def calculate_angle(a, b, c):
         angle = 360 - angle
 
     return angle
-def warning():
-    #audio = gTTS(text="Измените положение, вы сидите неправильно!", lang="ru")
-    #audio.save("example.mp3")
-    os.system("start example.mp3")
-    sleep(10)
 
 with mp_pose.Pose(min_detection_confidence=0.5, min_tracking_confidence=0.5) as pose:  #объявление переменной pose как скелетной модели с точностью обнаружения 0.5
     while cap.isOpened(): #пока камера открыта
         ret, frame = cap.read() #считывание кадров с камеры
-        fps = cap.get(cv2.CAP_PROP_FPS)#частота кадров в секунду
         image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)  #перевод изображений в RGB
         image.flags.writeable = False  #не сохраняем изображения
         results = pose.process(image)  #массив результатов
@@ -51,6 +47,12 @@ with mp_pose.Pose(min_detection_confidence=0.5, min_tracking_confidence=0.5) as 
             shoulderL = [landmarks[mp_pose.PoseLandmark.LEFT_SHOULDER.value].x, landmarks[mp_pose.PoseLandmark.LEFT_SHOULDER.value].y]
             shoulderR = [landmarks[mp_pose.PoseLandmark.RIGHT_SHOULDER.value].x, landmarks[mp_pose.PoseLandmark.RIGHT_SHOULDER.value].y]
 
+            wristL = [landmarks[mp_pose.PoseLandmark.LEFT_WRIST.value].x, landmarks[mp_pose.PoseLandmark.LEFT_WRIST.value].y]
+            wristR = [landmarks[mp_pose.PoseLandmark.RIGHT_WRIST.value].x,landmarks[mp_pose.PoseLandmark.RIGHT_WRIST.value].y]
+
+            elbowL = [landmarks[mp_pose.PoseLandmark.LEFT_ELBOW.value].x, landmarks[mp_pose.PoseLandmark.LEFT_ELBOW.value].y]
+            elbowR = [landmarks[mp_pose.PoseLandmark.RIGHT_ELBOW.value].x,landmarks[mp_pose.PoseLandmark.RIGHT_ELBOW.value].y]
+
             earL = [landmarks[mp_pose.PoseLandmark.LEFT_EAR.value].x, landmarks[mp_pose.PoseLandmark.LEFT_EAR.value].y]
             earR = [landmarks[mp_pose.PoseLandmark.RIGHT_EAR.value].x, landmarks[mp_pose.PoseLandmark.RIGHT_EAR.value].y]
 
@@ -62,47 +64,53 @@ with mp_pose.Pose(min_detection_confidence=0.5, min_tracking_confidence=0.5) as 
 
             ankleL = [landmarks[mp_pose.PoseLandmark.LEFT_ANKLE.value].x, landmarks[mp_pose.PoseLandmark.LEFT_ANKLE.value].y]
             ankleR = [landmarks[mp_pose.PoseLandmark.RIGHT_ANKLE.value].x, landmarks[mp_pose.PoseLandmark.RIGHT_ANKLE.value].y]
-
-           # угол наклона ног
-            angleLL = calculate_angle(ankleL, kneeL, hipL)
-            cv2.putText(image, str(angleLL), tuple(np.multiply(kneeL, [640, 480]).astype(int)), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2, cv2.LINE_AA)
-
-            # угол наклона торса
-            angleHL = calculate_angle(kneeL, hipL, shoulderL)
-            cv2.putText(image, str(angleHL), tuple(np.multiply(hipL, [640, 480]).astype(int)), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2, cv2.LINE_AA)
-
             # отрисовка средней точки плечевого пояса
             avg_point_shoulder = ((shoulderL[0] + shoulderR[0]) / 2, (shoulderL[1] + shoulderR[1]) / 2)
-            cv2.circle(image, (int(avg_point_shoulder[0] * frame.shape[1]), int(avg_point_shoulder[1] * frame.shape[0])), 5, (0, 0, 255), -1)
+            cv2.circle(image,(int(avg_point_shoulder[0] * frame.shape[1]), int(avg_point_shoulder[1] * frame.shape[0])), 5,(0, 0, 255), -1)
             # отрисовка средней точки тазового пояса
             avg_point_hip = ((hipL[0] + hipR[0]) / 2, (hipL[1] + hipR[1]) / 2)
-            cv2.circle(image,(int(avg_point_hip[0] * frame.shape[1]), int(avg_point_hip[1] * frame.shape[0])), 5, (0, 0, 255), -1)
+            cv2.circle(image, (int(avg_point_hip[0] * frame.shape[1]), int(avg_point_hip[1] * frame.shape[0])), 5,(0, 0, 255), -1)
 
-            cv2.line(image, (int(avg_point_shoulder[0] * frame.shape[1]), int(avg_point_shoulder[1] * frame.shape[0])),
-                     (int(avg_point_hip[0] * frame.shape[1]), int(avg_point_hip[1] * frame.shape[0])), (255, 0, 0), 2)
+            #вычисление угла подъема рук
+            angleEL = calculate_angle(shoulderL, elbowL, wristL)
+            #cv2.putText(image, str(angleEL), tuple(np.multiply(elbowL, [640, 480]).astype(int)),cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2, cv2.LINE_AA)
+            angleER = calculate_angle(shoulderR, elbowR, wristR)
+            #cv2.putText(image, str(angleER), tuple(np.multiply(shoulderR, [640, 480]).astype(int)),cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2, cv2.LINE_AA)
+
+            # угол наклона ног
+            angleLL = calculate_angle(ankleL, kneeL, hipL)
+            #cv2.putText(image, str(angleLL), tuple(np.multiply(kneeL, [640, 480]).astype(int)), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2, cv2.LINE_AA)
+            angleLR = calculate_angle(ankleR, kneeR, hipR)
+            #cv2.putText(image, str(angleLR), tuple(np.multiply(ankleR, [640, 480]).astype(int)),cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2, cv2.LINE_AA)
+
+            # угол наклона торса
+            avg_point_kneel = ((kneeL[0] + kneeR[0]) / 2, (kneeL[1] + kneeR[1]) / 2)
+            angleHL = calculate_angle(avg_point_kneel, avg_point_hip, avg_point_shoulder)
+            #cv2.putText(image, str(angleHL), tuple(np.multiply(hipL, [640, 480]).astype(int)), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2, cv2.LINE_AA)
+
+            #cv2.line(image, (int(avg_point_shoulder[0] * frame.shape[1]), int(avg_point_shoulder[1] * frame.shape[0])),
+             #        (int(avg_point_hip[0] * frame.shape[1]), int(avg_point_hip[1] * frame.shape[0])), (255, 0, 0), 2)
 
             avg_point_ear = ((earL[0] + earR[0]) / 2, (earL[1] + earR[1]) / 2)
             cv2.circle(image, (int(avg_point_ear[0] * frame.shape[1]), int(avg_point_ear[1] * frame.shape[0])), 5, (0, 0, 255), -1)
-
+            #угол наклона шеи
             angleL = calculate_angle(avg_point_ear, avg_point_shoulder, avg_point_hip)
-            cv2.putText(image, str(angleL), tuple(np.multiply(avg_point_shoulder, [640, 480]).astype(int)), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2, cv2.LINE_AA)
+            #cv2.putText(image, str(angleL), tuple(np.multiply(avg_point_shoulder, [640, 480]).astype(int)), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2, cv2.LINE_AA)
 
-            if angleL > 150 and (angleHL > 85 and angleHL < 115) and (angleLL > 80 and angleLL < 105):
+            if angleL > 155 and (angleHL > 85 and angleHL < 115) and (angleLL > 80 and angleLL < 120) and (angleLR > 80 and angleLR < 120) and angleEL > 25 and angleER > 25:
                 stage = "good"
                 bad_frames = 0
                 good_frames += 1
-            if angleL < 150 or (angleHL < 85 or angleHL > 115) or (angleLL < 80 or angleLL > 105) and stage == 'good':
+            if angleL < 155 or (angleHL < 85 or angleHL > 110) or (angleLL < 80 or angleLL > 120) or (angleLR < 80 or angleLR > 120) or angleEL < 25 or angleER < 25 and stage == 'good':
                 stage = "bad"
                 bad_frames += 1
                 good_frames = 0
 
-            #good_time = (1 / fps) * good_frames
-            bad_time = (1 / fps) * bad_frames
+            if bad_frames > bad_time:
+                os.system("start example.mp3")
+                bad_frames = 0
 
-            #if bad_time > 15:
-                #warning()
-                #os.system("start example.mp3")
-                #sleep(10)
+
         except:
             pass
         #прямоугольник состояния: начальные координаты, размер, цвет, заполнение цветом (-1))
@@ -111,6 +119,7 @@ with mp_pose.Pose(min_detection_confidence=0.5, min_tracking_confidence=0.5) as 
         #вывод состояния сгибания (вверх, вниз)
         cv2.putText(image, 'POSE', (80, 20), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 0), 1, cv2.LINE_AA)
         cv2.putText(image, stage, (70, 60), cv2.FONT_HERSHEY_SIMPLEX, 1.2, (255, 255, 255), 1, cv2.LINE_AA)
+
         mp_drawing.draw_landmarks(image, results.pose_landmarks, mp_pose.POSE_CONNECTIONS,
                                   mp_drawing.DrawingSpec(color=(245, 117, 66), thickness=2, circle_radius=2),
                                   mp_drawing.DrawingSpec(color=(245, 66, 230), thickness=2, circle_radius=2)
